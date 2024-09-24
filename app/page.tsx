69a6c69bac50264466e2870489caa55a37d9123a
Nikhil Mithani <31946890+Minikhil@users.noreply.github.com>
2024-09-24 03:51:52 +0000

    [FEAT] Move category input to game session page, reducing user friction to get game started

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,6 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import {LoaderComponent} from "./components/loader";
-import Page from "./[gameId]/page";
-import { pages } from "next/dist/build/templates/app-page";
 
 Amplify.configure(outputs);
 
@@ -25,14 +23,6 @@ type Card = {
 
 export default function App() {
   const router = useRouter();
-  const [categories, setCategories] = useState({
-    category1: "",
-    category2: "",
-    category3: "",
-    category4: "",
-    category5: "",
-  });
-  const [words, setWords] = useState<string[]>([]);
 
   //setGameState is function which updates gameState and returns of type gameState 
   const [gameState, setGameState] = useState<{
@@ -52,7 +42,6 @@ export default function App() {
   });
 
   const [loading, setLoading] = useState(false);
-  const [spymasterView, setSpymasterView] = useState(false);
 
   const handleGameIDChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGameState((prevState) => ({
@@ -61,90 +50,15 @@ export default function App() {
     }));
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = e.target;
-    setCategories((prevCategories) => ({
-      ...prevCategories,
-      [id]: value,
-    }));
-  };
-
-  async function handleGenerateWords() {
+  async function routeToGamePage() {
     if (gameState.gameId === undefined) {
       alert("Please enter game ID.");
       return;
     }
 
-    const categoryValues = Object.values(categories).filter((cat) => cat.trim() !== "");
-  
-    if (categoryValues.length < 5) {
-      alert("Please enter all 5 categories.");
-      return;
-    }
-  
     setLoading(true);
-  
-    try {
-      const response = await fetch("/api/openai", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ categories: categoryValues }),
-      });
-  
-      if (!response.ok) {
-        throw new Error("Failed to fetch words");
-      }
-  
-      const generatedWords = await response.json();
-      const parsedWords = JSON.parse(generatedWords);
-  
-      if (Array.isArray(parsedWords)) {
-        const flatWords = parsedWords.flat();
-        setWords(flatWords);
-        const newCards = createBoard(flatWords);
-  
-        /**
-         * Wrap the state update in a Promise to ensure it gets completed. This is needed 
-           because Reacts setState function are async so once we update it it may not reflect right away 
-           since we are passing it to insertNewGameToDynamo we need to wait to ensure updated value is being passed 
-         */
-        const updatedState = await new Promise<typeof gameState>((resolve) => {
-
-          // Update gameState and return 
-          setGameState(prevState => {
-            const updatedState = {
-              ...prevState,
-              cards: newCards,
-              totalCardsLeft: 25,
-              redCardsLeft: 9,
-              blueCardsLeft: 8,
-            };
-
-            //resolve the promise 
-            resolve(updatedState);
-
-            //React expects the function to return the new state value when using functional setState
-            return updatedState;
-          });
-
-        });
-        
-        // Promise resolved, insert into DynamoDB and wait for it to complete before routing 
-        await insertNewGameToDynamo(updatedState);
-        
-        console.log("ROUTING!");
-        router.push(`/${gameState.gameId}`);
-      } else {
-        throw new Error("Invalid format of generated words");
-      }
-    } catch (error) {
-      console.error("Error generating words:", error);
-      alert("There was an error generating words. Please try again.");
-    } finally {
-      setLoading(false);
-    }
+    console.log("ROUTING!");
+    router.push(`/${gameState.gameId}`);
   }
 
   function createBoard(words: string[]): Card[] {
@@ -215,15 +129,7 @@ export default function App() {
           value={gameState.gameId}
           onChange={handleGameIDChange}
         />
-      </div>
-
-      <div className="category-input">
-        <input type="text" id="category1" placeholder="Category 1" value={categories.category1} onChange={handleChange} />
-        <input type="text" id="category2" placeholder="Category 2" value={categories.category2} onChange={handleChange} />
-        <input type="text" id="category3" placeholder="Category 3" value={categories.category3} onChange={handleChange} />
-        <input type="text" id="category4" placeholder="Category 4" value={categories.category4} onChange={handleChange} />
-        <input type="text" id="category5" placeholder="Category 5" value={categories.category5} onChange={handleChange} />
-        <Button variation="primary" colorTheme="success" onClick={handleGenerateWords}>Start</Button>
+        <Button variation="primary" colorTheme="success" onClick={routeToGamePage}>Start</Button>
       </div>
 
       {loading &&  <LoaderComponent />}
